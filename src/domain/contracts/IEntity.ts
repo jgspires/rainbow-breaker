07fe9632ff574b43b3ce3a32fb39d278c435c9ea
joao.pires1 <joao.pires@sflabs.com.br>
2022-05-19 20:10:24 -0300

    refactor(entities): move spritesheet data to entity

--- src/domain/contracts/IEntity.ts
+++ src/domain/contracts/IEntity.ts
@@ -1,8 +1,10 @@
 import { Dimensions, Position } from '../entities/components'
+import { SpriteSheetData } from '../entities/engine'
 
 export interface IEntity {
   position: Position
   dimensions: Dimensions
+  spriteSheetData: SpriteSheetData
   draw(context: CanvasRenderingContext2D): void
   update(): void
 }
--- src/domain/contracts/blocks/IBlock.ts
+++ src/domain/contracts/blocks/IBlock.ts
@@ -6,13 +6,11 @@ export type BlockProps = {
   maxHealth: number
   currentHealth: number
   colour: string
-  spriteSheetData: SpriteSheetData
 }
 
 export type BlockPropChanges = {
   maxHealth?: number
   colour?: string
-  spriteSheetData?: SpriteSheetData
 }
 
 export interface IBlock extends IEntity, IBoxCollidable {
