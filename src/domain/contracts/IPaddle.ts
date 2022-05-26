4263f103391c757d175110422adaaa97da53524b
joao.pires1 <joao.pires@sflabs.com.br>
2022-05-26 00:03:54 -0300

    feat(entities): finish paddle logic

--- src/domain/contracts/IPaddle.ts
+++ src/domain/contracts/IPaddle.ts
@@ -1,12 +1,18 @@
+import { GameDirection } from '../entities/engine'
 import { IBoxCollidable } from './IBoxCollidable'
 import { IEntity } from './IEntity'
 import { IFrameAnimated } from './IFrameAnimated'
 
+export type PaddleDirection = 'left' | 'right'
+
 export type PaddleProps = {
+  currentVelocity: number
   maxVelocity: number
   acceleration: number
+  deceleration: number
 }
 
 export interface IPaddle extends IEntity, IBoxCollidable, IFrameAnimated {
   paddleProps: PaddleProps
+  accelerate(direction: PaddleDirection): void
 }
--- src/domain/entities/Paddle.ts
+++ src/domain/entities/Paddle.ts
@@ -1,4 +1,4 @@
-import { AnimationProps, IPaddle, PaddleProps } from '../contracts'
+import { AnimationProps, IPaddle, PaddleDirection, PaddleProps } from '../contracts'
 import { Position, Dimensions, Hitbox } from './components'
 import { SpriteSheetData } from './engine'
 import { SpriteHelper } from './utils'
@@ -12,8 +12,10 @@ export class Paddle implements IPaddle {
     maxFrame: 360
   }
   paddleProps: PaddleProps = {
-    acceleration: 1.0,
-    maxVelocity: 5
+    currentVelocity: 0.0,
+    acceleration: 1.5,
+    deceleration: 0.25,
+    maxVelocity: 5.0
   }
   spriteSheetData: SpriteSheetData = {
     spriteSheetImage: document.getElementById('paddle-sprite-sheet')! as HTMLImageElement,
@@ -31,36 +33,68 @@ export class Paddle implements IPaddle {
     this.hitbox = new Hitbox(this.position, this.dimensions)
   }
 
-  update(): void {
-    console.log(`updating paddle ${this}`)
-    this.animationProps.currentFrame++
+  accelerate(direction: PaddleDirection): void {
+    if (
+      (direction === 'right' && this.paddleProps.currentVelocity < 0) ||
+      (direction === 'left' && this.paddleProps.currentVelocity > 0)
+    )
+      this.paddleProps.currentVelocity = 0
 
-    if (this.animationProps.currentFrame > this.animationProps.maxFrame)
-      this.animationProps.currentFrame = 0
+    if (direction === 'right') this.paddleProps.currentVelocity += this.paddleProps.acceleration
+    else this.paddleProps.currentVelocity -= this.paddleProps.acceleration
+
+    if (this.paddleProps.currentVelocity > this.paddleProps.maxVelocity)
+      this.paddleProps.currentVelocity = this.paddleProps.maxVelocity
+    else if (this.paddleProps.currentVelocity < this.paddleProps.maxVelocity * -1)
+      this.paddleProps.currentVelocity = this.paddleProps.maxVelocity * -1
+  }
+
+  update(): void {
+    this.animate()
+    this.move()
+  }
+
+  move(): void {
+    console.log(this.paddleProps.currentVelocity)
+    if (this.paddleProps.currentVelocity > 0)
+      this.paddleProps.currentVelocity = Math.max(
+        this.paddleProps.currentVelocity - this.paddleProps.deceleration,
+        0
+      )
+    else if (this.paddleProps.currentVelocity < 0)
+      this.paddleProps.currentVelocity = Math.min(
+        this.paddleProps.currentVelocity + this.paddleProps.deceleration,
+        0
+      )
+
+    this.position.x = this.position.x + this.paddleProps.currentVelocity
   }
 
   collide(): void {
     throw new Error('Method not implemented.')
   }
 
+  animate(): void {
+    this.animationProps.currentFrame++
+
+    if (this.animationProps.currentFrame > this.animationProps.maxFrame)
+      this.animationProps.currentFrame = 0
+  }
+
   draw(context: CanvasRenderingContext2D): void {
     const spriteIndex = this.getPaddleSprite()
     const spriteStartPos = new SpriteHelper().getSpriteStartPos(spriteIndex, this.spriteSheetData)
-    context.drawImage(
-      this.spriteSheetData.spriteSheetImage,
-      spriteStartPos.x,
-      spriteStartPos.y,
-      this.spriteSheetData.spriteSize.width,
-      this.spriteSheetData.spriteSize.height,
-      this.position.x,
-      this.position.y,
-      this.dimensions.width,
-      this.dimensions.height
+    SpriteHelper.drawSprite(
+      spriteStartPos,
+      this.position,
+      this.dimensions,
+      this.spriteSheetData,
+      context
     )
   }
 
   getPaddleSprite(): number {
-    const animationState = SpriteHelper.getAnimationCycleState(this.animationProps, 6)
+    const animationState = SpriteHelper.getAnimationCycleSprite(this.animationProps, 6)
     if (animationState === 1 || animationState === 7) return 0
     else if (animationState === 2 || animationState === 6) return 1
     else if (animationState === 3 || animationState === 5) return 2
